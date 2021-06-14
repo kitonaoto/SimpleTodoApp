c33024c613ebdcd75d8238fe18eaf7406d1f55ac
kitonaoto <kitonaoto@team-lab.com>
2021-06-14 20:38:10 +0900

    xmlファイル名前変更

--- app/src/main/java/com/example/simpletodoapp/IndexTodo.kt
+++ app/src/main/java/com/example/simpletodoapp/IndexTodo.kt
@@ -18,7 +18,7 @@ class IndexTodo : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_index_todo, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/simpletodoapp/NewTodo.kt
+++ app/src/main/java/com/example/simpletodoapp/NewTodo.kt
@@ -18,7 +18,7 @@ class NewTodo : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_new_todo, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
